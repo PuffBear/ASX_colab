d9d08dddfa62afd60c98069de762ebbb0a96a696
Agriya Yadav <agriyayadav@gmail.com>
2025-02-19 07:52:58 +0530

    defined the Order Class.

--- OrderBookSystem/orderbook_struct.py
+++ OrderBookSystem/orderbook_struct.py
@@ -23,3 +23,40 @@ quote = {'type' : 'market',
          'trade_id' : 002}
 
 '''
+import time, random, itertools
+
+'''
+Defining individual orders.
+'''
+class Order:
+    # itertools used to generate unique, incremental Order IDs without explicitly tracking them
+    # this process has an efficient O(1) ID generation, its Thread-Safe, No risk of duplicate orders
+    # even if we end up using an SQL database, this can be used to avoid generating an idea at the 
+    # time of insertion of the order in the database
+    order_counter = itertools.count(1)
+
+    def __init__(self, orderSide, price, quantity, orderType):
+        # define the order's unique order id
+        self.orderId = next(Order.order_counter)
+        # is it a BUY or SELL -side order?
+        self.side = orderSide
+        self.price = price
+        self.quantity = quantity
+        # is it a Limit, Market, Stop order
+        self.orderType = orderType
+        # what is the time when the order is placed.
+        self.timestamp = time.time()
+        # for the DLL-FIFO implementation
+        self.next = None
+        self.prev = None
+
+    # how to represent an order in the form of a string:
+    def __repr__(self):
+        return f"Order(ID={self.orderId}, {self.side}, {self.quantity}@{self.price})"
+    
+'''
+FIFO execution at the DLL level:
+'''
+class OrderList:
+    def __init__(self):
+        pass
